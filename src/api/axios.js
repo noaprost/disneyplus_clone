04b182874a05a16780527a9b1baac7483a8bcd33
noaprost <kangjiwon1850@gmail.com>
2024-10-23 21:01:13 +0900

    axios 파일 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-
 // axios 인스턴스 생성
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3", // 공통 baseURL 설정
   params: {
-    api_key: API_KEY, // 모든 요청에 API_KEY를 자동으로 포함시킴
+    api_key: process.env.REACT_APP_TMDB_API_KEY,
   },
   headers: {
     "Content-Type": "application/json", // 요청의 기본 헤더 설정
   },
-  timeout: 5000, // 타임아웃 설정 (옵션)
+  timeout: 5000, // 타임아웃 설정
 });
 
 // 인스턴스 내보내기
