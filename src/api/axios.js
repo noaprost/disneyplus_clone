22c6d6ff90f3ee6238ab52046e3be0ef0a867560
noaprost <kangjiwon1850@gmail.com>
2024-10-23 21:23:40 +0900

    manifast 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+
 // axios 인스턴스 생성
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3", // 공통 baseURL 설정
   params: {
-    api_key: process.env.REACT_APP_TMDB_API_KEY,
+    api_key: API_KEY,
   },
   headers: {
     "Content-Type": "application/json", // 요청의 기본 헤더 설정
@@ -12,5 +14,4 @@ const instance = axios.create({
   timeout: 5000, // 타임아웃 설정
 });
 
-// 인스턴스 내보내기
 export default instance;
